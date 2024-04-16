41f618b6cbf4bbabb79753a11dfe3b44a0554568
Richard Abbuhl <Richard.Abbuhl@Rabobank.nl>
2024-04-16 11:48:27 +0200

    Initial commit.

---
+++ src/main/java/com/example/raopenaihelloworld/RaOpenaiHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.raopenaihelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaOpenaiHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaOpenaiHelloworldApplication.class, args);
+	}
+
+}
